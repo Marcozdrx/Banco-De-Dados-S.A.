329e6fa4536a22d7ca4c84bd4703d1d34fb44019
Marcos Paulo Fernandes <gameeerluk@gmail.com>
2025-05-12 16:23:09 -0300

    Update consulta com inner join - 1.sql

--- consulta com inner join - 1.sql
+++ consulta com inner join - 1.sql
@@ -1,9 +1,12 @@
 -- Listar os nomes dos usuários, os nomes dos livros emprestados e as datas de empréstimo
 
+-- Selecionando os dados para ver na consulta, e nomeando-os 
 SELECT 
-    u.nomeusuario AS Usuario,
-    l.nome AS Livro,
-    e.data_emprestimo AS DataEmprestimo
-FROM usuario u
-INNER JOIN emprestimo e ON u.Cod_Emprestimo = e.Cod_Emprestimo
-INNER JOIN livro l ON e.Cod_Livro = l.Cod_Livro;
+    usuario.nomeusuario AS Usuario,
+    livro.nome AS Livro,
+    emprestimo.data_emprestimo AS DataEmprestimo
+
+-- Juntando as tabelas e dando as condições para evitar redundancias
+FROM usuario 
+INNER JOIN emprestimo  ON usuario.Cod_Emprestimo = emprestimo.Cod_Emprestimo
+INNER JOIN livro ON emprestimo.Cod_Livro = livro.Cod_Livro;
